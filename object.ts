08a48ce3a1c8b30049f50379e6d78052bf2b5a39
ariadna <ariadna>
2024-10-11 11:29:40 +0300

    add: car object

---
+++ object.ts
@@ -0,0 +1,11 @@
+const machineCharacteristic ={
+    salon:'dark',
+    safetySystems: 'ABS',
+    cushions: 'In front',
+    headlamps: 'Xenon ones',
+    heater: 'The seats',
+}
+
+machineCharacteristic.salon = 'dark'
+
+console.log(machineCharacteristic)
---
+++ variable.ts
@@ -0,0 +1,5 @@
+const first_number=1
+const second_number=8896
+const result=first_number+second_number
+
+console.log(result)
