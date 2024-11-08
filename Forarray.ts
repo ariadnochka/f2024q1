b3efd3740efe33ab176b931c562a1209cbe9bec0
ariadna <ariadna>
2024-11-08 11:07:55 +0300

    add:functionsum

---
+++ Forarray.ts
@@ -0,0 +1,9 @@
+const arrayfruits = ['banana','apple','raspberry','strawberry','cherry']
+
+for (let y = 0; y < arrayfruits.length; y++) {
+    if (arrayfruits[y] === 'banana') {
+        console.log(`в данном массиве найден $(arrayfruits[y]}`)
+    }
+}
+
+   
